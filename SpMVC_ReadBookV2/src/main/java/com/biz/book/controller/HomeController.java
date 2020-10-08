0b88926e833d48bd2941a2958fb58a317150b923
heonjunkim <maplegjswns@naver.com>
2020-10-08 16:42:49 +0900

    추가

--- SpMVC_ReadBookV2/src/main/java/com/biz/book/controller/HomeController.java
+++ SpMVC_ReadBookV2/src/main/java/com/biz/book/controller/HomeController.java
@@ -9,16 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.biz.book.model.SelectMap;
+
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model) {	
 		return "home";
 	}
-
+	
+	@RequestMapping(value="/select", method=RequestMethod.GET)
+	public String select(Model model) {
+		
+		model.addAttribute("CAT_MAP",SelectMap.CAT_MAP);
+		model.addAttribute("FLAG_MAP",SelectMap.FLAG_MAP);
+		model.addAttribute("category","CAT_PROMISE");
+		return "home";
+	}
 }
